eaab59844699aabf2a702710de5c580e73f792af
Whale Mo <ncwhale@gmail.com>
2022-01-09 21:39:17 +0900

    Use method object instead of switch case.

--- index.js
+++ index.js
@@ -18,44 +18,54 @@ app.use(
   })
 );
 
-app.use(async (ctx) => {
-  // TODO: use route or object.
-  switch (ctx.method) {
-    case "GET":
-      {
-        let origin_url = storage.get(ctx.url.slice(1));
-        if (origin_url) {
-          ctx.redirect(origin_url);
-          ctx.body = origin_url;
-        } else {
-          ctx.status = 204;
-        }
-      }
-      break;
-    case "POST":
-    case "PUT":
-      {
-        // Use POST/PUT to create a shorten URL.
-        let origin_url = ctx.request.body.url || ctx.header.url;
-        // Check URL.
-        origin_url = new URL(origin_url);
+function CreateShortURL(ctx) {
+  try {
+    // URL param can from form data or http header.
+    let origin_url = ctx.request.body.url || ctx.header.url;
+    // Check URL.
+    origin_url = new URL(origin_url);
 
-        let short_url = storage.create(origin_url.toString());
-        ctx.body = ctx.origin + "/" + short_url;
-      }
-      break;
-    case "PATCH":
-      // Use PATCH to modify a shorten URL.
-      break;
-    case "DELETE":
-      // Use DELETE to delete a shorten URL.
-      {
-        ctx.body = "Delete url";
-      }
-      break;
-    default:
-      ctx.redirect("https://google.com");
-      break;
+    let short_url = storage.create(origin_url.toString());
+    ctx.body = ctx.origin + "/" + short_url;
+  } catch (e) {
+    // TODO: handle error.
+    console.error(e);
+    // silently ignore.
+    ctx.status = 204;
+  }
+}
+
+function GetShortURL(ctx) {
+  let origin_url = storage.get(ctx.url.slice(1));
+  if (origin_url) {
+    ctx.redirect(origin_url);
+    ctx.body = origin_url;
+  } else {
+    ctx.status = 204;
+  }
+}
+
+function ModifyShortURL(ctx) {
+  ctx.body = "Modify url";
+}
+
+function DeleteShortURL(ctx) {
+  ctx.body = "Deleted url";
+}
+
+const method = {
+  GET: GetShortURL,
+  PUT: CreateShortURL,
+  POST: CreateShortURL,
+  PATCH: ModifyShortURL,
+  DELETE: DeleteShortURL,
+};
+
+app.use(async (ctx) => {
+  if (ctx.method in method) {
+    method[ctx.method](ctx);
+  } else {
+    ctx.status = 204;
   }
 });
 
