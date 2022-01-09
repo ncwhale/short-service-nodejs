712d5aea984146aa63d0ccd2add57fc89579b3ac
Whale Mo <ncwhale@gmail.com>
2022-01-09 22:39:05 +0900

    use ctx.short_url insteadof ctx.url.slice(1) everywhere.

--- index.js
+++ index.js
@@ -24,18 +24,18 @@ async function CreateShortURL(ctx) {
   // Check URL.
   origin_url = new URL(origin_url);
 
-  let short_url = ctx.url.slice(1);
+  let short_url = ctx.short_url;
   // TODO: use config instead of magic number.
   if (short_url.length < 8) {
     short_url = null;
   }
 
-  let short_url = await storage.create(origin_url.toString(), { short_url });
+  short_url = await storage.create(origin_url.toString(), { short_url });
   ctx.body = ctx.origin + "/" + short_url;
 }
 
 async function GetShortURL(ctx) {
-  let origin_url = await storage.get(ctx.url.slice(1));
+  let origin_url = await storage.get(ctx.short_url);
   if (origin_url) {
     ctx.redirect(origin_url);
     ctx.body = origin_url;
@@ -45,8 +45,7 @@ async function GetShortURL(ctx) {
 }
 
 async function ModifyShortURL(ctx) {
-  let short_url = ctx.url.slice(1);
-  if (short_url.legnth < 1) {
+  if (ctx.short_url.legnth < 1) {
     // silently ignore.
     ctx.status = 204;
     return;
@@ -56,13 +55,13 @@ async function ModifyShortURL(ctx) {
   let origin_url = ctx.request.body.url || ctx.header.url;
   origin_url = new URL(origin_url);
 
-  let result = await storage.modify(short_url, origin_url.toString());
+  let result = await storage.modify(ctx.short_url, origin_url.toString());
 
   ctx.body = "Modified";
 }
 
 async function DeleteShortURL(ctx) {
-  let result = await storage.delete(ctx.url.slice(1));
+  let result = await storage.delete(ctx.short_url);
   ctx.body = "Deleted";
 }
 
@@ -77,6 +76,8 @@ const method = {
 app.use(async (ctx) => {
   if (ctx.method in method)
     try {
+      // Every method need this param.
+      ctx.short_url = ctx.url.slice(1);
       await method[ctx.method](ctx);
     } catch (e) {
       // TODO: handle error.
