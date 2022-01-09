fdb453c5974e49fe53f8617a0c0217eccb017727
Whale Mo <ncwhale@gmail.com>
2022-01-09 19:53:54 +0900

    Add get impl.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const Koa = require("koa");
 const app = new Koa();
+// TODO: use config file.
+const Storage = require("./storage/raw-file");
+const storage = new Storage("./url-shortener.txt");
 
 app.use(async (ctx) => {
   console.log(ctx.method);
+  console.log(ctx.url);
+
+  // TODO: use route or object.
   switch (ctx.method) {
     case "GET":
-      ctx.body = "Hello World";
+      let origin_url = storage.get(ctx.url.slice(1));
+      if (origin_url) {
+        ctx.redirect(origin_url);
+        ctx.body = origin_url;
+      } else {
+        ctx.status = 204;
+      }
       break;
     case "POST":
     case "PUT":
