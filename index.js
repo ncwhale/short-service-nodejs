a3b9a0f73514bb2523266d2d4c193de9f40e49d5
Whale Mo <ncwhale@gmail.com>
2022-01-11 19:21:22 +0900

    Add development config print for debug.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const config = require("config");
 const app = require("./app");
 
+// For development debugging.
+if(config.util.getEnv("NODE_ENV") === "development") {
+  console.dir(config);
+}
+
 if (config.has("service.host")) {
   app.listen(config.get("service.port"), config.get("service.host"));
 } else {
