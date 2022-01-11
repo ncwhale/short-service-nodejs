9004a8231de4a63541644adf2521f312b7f20c97
Whale Mo <ncwhale@gmail.com>
2022-01-11 20:37:28 +0900

    Split shorturl service to shorturl.js.

--- app/index.js
+++ app/index.js
@@ -1,95 +1,13 @@
 "use strict";
 
-const config = require("config");
 const Koa = require("koa");
-const KoaBody = require("koa-body");
-const KoaRouter = require("@koa/router");
-const URL = require("url").URL;
-
-const storage = require("../storage");
-const auth = require("../auth");
 
 const app = new Koa();
-const router = new KoaRouter();
-const bodyParser = KoaBody({
-  multipart: false,
-  urlencoded: true,
-  json: true,
-  text: true,
-  encoding: "utf8",
-  parsedMethods: ["POST", "PUT", "PATCH"],
-});
 
-async function CreateShortURL(ctx) {
-  // URL param can from form data or http header.
-  let origin_url = ctx.request.body.url || ctx.header.url;
-  // Check URL.
-  origin_url = new URL(origin_url);
-
-  let short_url = ctx.params.short_url || "";
-  if (
-    short_url.length < config.get("short_url.predefined_min_size") ||
-    short_url.length > config.get("short_url.predefined_max_size")
-  ) {
-    short_url = null;
-  }
-  let short_url_size = config.get("short_url.random_size");
-
-  let result = await storage.create(origin_url.toString(), {
-    short_url,
-    short_url_size,
-  });
-  ctx.body = {
-    ...result,
-    short_url: ctx.origin + "/" + result.short_url,
-  };
-}
-
-async function GetShortURL(ctx) {
-  let origin_url = await storage.get(ctx.params.short_url);
-  if (origin_url) {
-    ctx.redirect(origin_url);
-    ctx.body = { origin_url };
-  } else {
-    ctx.status = 204;
-  }
-}
-
-async function ModifyShortURL(ctx) {
-  // Check URL.
-  let origin_url = ctx.request.body.url || ctx.header.url;
-  origin_url = new URL(origin_url);
-
-  let result = await storage.modify(ctx.params.short_url, origin_url.toString());
-
-  ctx.body = "Modified";
-}
-
-async function DeleteShortURL(ctx) {
-  let result = await storage.delete(ctx.params.short_url);
-  ctx.body = "Deleted";
-}
-
-// Error handler.
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (e) {
-    // TODO: handle error.
-    console.error(e);
-    // silently ignore.
-    ctx.status = 204;
-  }
-});
-
-router.get("/:short_url", GetShortURL);
-router.post("/", auth.check, bodyParser, CreateShortURL);
-router.put("/:short_url", auth.check, bodyParser, CreateShortURL);
-router.patch("/:short_url", auth.strict, bodyParser, ModifyShortURL);
-router.delete("/:short_url", auth.strict, DeleteShortURL);
+const shortURLrouter = require("./shorturl");
 
 app
-  .use(router.routes())
-  .use(router.allowedMethods());
+  .use(shortURLrouter.routes())
+  .use(shortURLrouter.allowedMethods());
 
 module.exports = app;
