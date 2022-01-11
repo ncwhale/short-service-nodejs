439c5ee88b29488285131a7212640ac108783a18
Whale Mo <ncwhale@gmail.com>
2022-01-11 21:36:34 +0900

    Add strict mode for predefined url.

--- config/default.js
+++ config/default.js
@@ -4,10 +4,20 @@ module.exports = {
     options: {},
   },
   short_url: {
+    // strict when create predefined short URL.
+    // if true, only verifed user can create predefined short URL.
+    predefined_url_strict: false,
+
     // predefined short URL size limit.
     predefined_min_size: 8,
     predefined_max_size: 128,
     random_size: 8,
+
+    // create/modify/delete rate limit.
+    operation_rate_limit: {
+      duration: 60000, // 1 min
+      max: 10,
+    }
   },
   auth: {
     module: "dummy",
@@ -17,6 +27,8 @@ module.exports = {
     enabled: false,
   },
   service: {
+    // enable x-forwarded-* header.
+    proxy: true,
     port: 3000,
     host: "localhost",
   }
