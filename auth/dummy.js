f4c38243cacd03b6f4352c7567d4cb2e24870d35
Whale Mo <ncwhale@gmail.com>
2022-01-12 20:18:50 +0900

    dummy module will check presharedkey.

--- auth/dummy.js
+++ auth/dummy.js
@@ -1,20 +1,37 @@
 class DummyVerify {
   constructor(config) {
+    this.config = config || {};
+    this.presharedkey = this.config.presharedkey || "test-pre-shared-key";
   }
 
-  init(){}
+  init() {
+    // bind middleware to context
+    this.check = this.check.bind(this);
+    this.strict = this.strict.bind(this);
+  }
 
   // Check if it is a valid call.
-  check(ctx, next) {
+  async check(ctx, next) {
+    if ("verified" in ctx) {
+      return next();
+    }
+
+    const signature = ctx.request.headers["x-signature"] || ctx.query.signature;
+    const timestamp = ctx.request.headers["x-timestamp"] || ctx.query.timestamp;
+
     // set ctx.verified flag.
-    ctx.verified = true;
+    ctx.verified = signature === this.presharedkey && timestamp > 0;
     return next();
   }
 
   // It must be verified.
-  strict(ctx, next) {
-    ctx.verified = true;
-    return next();
+  async strict(ctx, next) {
+    await this.check(ctx, () => {});
+    if (ctx.verified) {
+      await next();
+    } else {
+      return ctx.throw(401, "Unauthorized");
+    }
   }
 }
 
